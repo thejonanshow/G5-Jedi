634c3138ec5b1c1709e3793c4b5814b8d9bc71c3
1337807 <jonan.scheffler@g5platform.com>
2011-05-19 10:09:06 -0700

    A long time ago, in a galaxy far away...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5jedi::Application.config.secret_token = '3f56ad26cd6caa1a6977444124f2781fbfb78e61629316ec032473fd2b38062b14c06d1cc6e0ec3b2068a9fd8aad782442f157509951e20a6bb664a38d15f11c'
